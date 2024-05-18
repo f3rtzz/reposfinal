b994e06460460bbefb9ad3fc265073ed30129c8f
f3rtzz <fermeluft@gmail.com>
2024-05-18 00:44:30 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<html lang="pt-br">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifólio pessoal</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1 style="font-size: 30px;">Portfólio Pessoal</h1>
+    </header>
+    <nav>
+        <a href="https://linktr.ee/luftkd">Linktree</a>
+    </nav>
+    <section>
+        <div class="profile-text">
+            <img src="img/ftt.jpg" alt="Foto">
+            <h2 style="margin-bottom: 10px;">Fernando Menuzzi Luft</h2>
+            <p>Sou um estudante versátil e dedicado, com experiência tanto em atendimento ao cliente em ambientes noturnos movimentados quanto em habilidades técnicas sólidas em hardware e programação. Durante meu tempo como garçom, aprendi a importância da paciência e da eficiência, especialmente em dias agitados. Além disso, possuo conhecimento em linguagens de marcação e programação, incluindo HTML, CSS, JavaScript, TypeScript e Node.JS, o que me permite desenvolver projetos de forma criativa e eficaz.
+            </div>
+        
+    </section>
+    <footer>
+        <p>&copy; Portifólio pessoal 2024. Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
